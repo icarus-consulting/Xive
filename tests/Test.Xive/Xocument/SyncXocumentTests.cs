d2b6a0c136fae666f0e69495e47b29e6dd33d7b4
Miro Tunk <miro.tunk@icarus-consult.de>
2019-02-12 13:12:01 +0100

    tests: Add tests

---
+++ tests/Test.Xive/Xocument/SyncXocumentTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Xive.Test;
+using Xunit;
+
+namespace Xive.Xocument
+{
+    public sealed class SyncXocumentTests
+    {
+        [Fact]
+        public void DeliversParallel()
+        {
+            var accesses = 0;
+            var xoc =
+                new SyncXocument("synced",
+                    new FkXocument(() =>
+                    {
+                        accesses++;
+                        Assert.Equal(1, accesses);
+                        accesses--;
+                        return
+                            new XDocument(
+                                new XElement("synced", new XText("here"))
+                            );
+                    })
+                );
+
+            Parallel.For(0, Environment.ProcessorCount << 4, (i) =>
+            {
+                xoc.Value("/synced/text()", "");
+            });
+        }
+    }
+}
