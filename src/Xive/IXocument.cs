d72ea101fede69a8ea48b019a1605f1d5b5923e0
Miro Tunk <miro.tunk@icarus-consult.de>
2019-02-12 10:23:17 +0100

    refactor: let xocument deliver its node

--- src/Xive/IXocument.cs
+++ src/Xive/IXocument.cs
@@ -1,14 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
 using Yaapii.Xambly;
 using Yaapii.Xml;
 
 namespace Xive
 {
-    public interface IXocument
+    public interface IXocument : IDisposable
     {
         IList<string> Values(string xpath);
         string Value(string xpath, string def);
         IList<IXML> Nodes(string xpath);
         void Modify(IEnumerable<IDirective> dirs);
+        XNode Node();
     }
 }
