12488f6941744779b68a3f30917e2eeda37db096
Miro Tunk <miro.tunk@icarus-consult.de>
2019-02-12 13:07:53 +0100

    tests: add tests

---
+++ tests/Test.Xive/Xocument/CachedXocumentTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+using Xive.Xocument;
+using Xunit;
+using Yaapii.Xambly;
+using Yaapii.Xml;
+
+namespace Xive.Test.Xocument
+{
+    public sealed class CachedXocumentTests
+    {
+        [Fact]
+        public void FillsCacheWithContent()
+        {
+            var cache = new Dictionary<string, XNode>();
+            new CachedXocument(
+                "buffered.xml",
+                new SimpleXocument("buffered"),
+                cache
+            ).Node();
+
+            Assert.Equal(
+                "<buffered />",
+                cache["buffered.xml"].ToString()
+            );
+        }
+
+        [Fact]
+        public void ReadsContentFromCache()
+        {
+            var reads = 0;
+            var cache = new Dictionary<string, XNode>();
+            var xoc =
+                new CachedXocument(
+                    "buffered.xml",
+                    new FkXocument(() =>
+                    {
+                        reads++;
+                        return new XDocument(new XElement("buffered"));
+                    }),
+                    cache
+                );
+            xoc.Node();
+            xoc.Node();
+
+            Assert.Equal(1, reads);
+        }
+
+        [Fact]
+        public void UpdatesCache()
+        {
+            var cache = new Dictionary<string, XNode>();
+            var xoc =
+                new CachedXocument(
+                    "buffered.xml",
+                    new SimpleXocument("buffered"),
+                    cache
+                );
+            xoc.Node();
+            xoc.Modify(
+                new Directives()
+                    .Xpath("/buffered")
+                    .Set("10 Minutes")
+            );
+
+            Assert.Equal(
+                "10 Minutes",
+                new XMLQuery(cache["buffered.xml"]).Values("/buffered/text()")[0]
+            );
+        }
+    }
+}
---
+++ tests/Test.Xive/Xocument/CellXocumentTests.cs
@@ -0,0 +1,40 @@
+﻿using Xive.Cell;
+using Xive.Xocument;
+using Xunit;
+using Yaapii.Xambly;
+
+namespace Xive.Test.Xocument
+{
+    public sealed class CellXocumentTests
+    {
+        [Fact]
+        public void DeliversContent()
+        {
+            Assert.Equal(
+                "<flash />",
+                new CellXocument(
+                    new RamCell("flash.xml"),
+                    "flash.xml"
+                ).Node().ToString()
+            );
+        }
+
+        [Fact]
+        public void UpdatesContent()
+        {
+            var xoc =
+                new CellXocument(
+                    new RamCell("flash.xml"),
+                    "flash.xml"
+                );
+            
+            xoc.Modify(
+                new Directives()
+                    .Xpath("/flash")
+                    .Add("grandmaster").Set("scratch scratch")
+            );
+
+            Assert.Equal("scratch scratch", xoc.Value("/flash/grandmaster/text()", ""));
+        }
+    }
+}
