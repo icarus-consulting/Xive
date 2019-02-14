34f1942cf8b42a09ebc57f6f2582ed5ec7d6bc3c
Miro Tunk <miro.tunk@icarus-consult.de>
2019-02-14 17:02:40 +0100

    tests: add verbose cell

---
+++ tests/Test.Xive/VerboseCell.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Yaapii.Atoms;
+
+namespace Xive.Test
+{
+    /// <summary>
+    /// A cell that reports when its methods are called.
+    /// </summary>
+    public sealed class VerboseCell : ICell
+    {
+        private readonly Action reportContent;
+        private readonly Action reportUpdate;
+        private readonly Action reportDispose;
+        private readonly ICell origin;
+
+        /// <summary>
+        /// A cell that reports when its methods are called.
+        /// </summary>
+        public VerboseCell(ICell origin, Action content, Action update, Action dispose)
+        {
+            this.reportContent = content;
+            this.reportUpdate = update;
+            this.reportDispose = dispose;
+            this.origin = origin;
+        }
+
+        public byte[] Content()
+        {
+            this.reportContent();
+            return this.Content();
+        }
+
+        public void Dispose()
+        {
+            this.reportDispose();
+            this.origin.Dispose();
+        }
+
+        public void Update(IInput content)
+        {
+            this.reportUpdate();
+            this.origin.Update(content);
+        }
+    }
+}
