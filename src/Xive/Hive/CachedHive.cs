32b4be824248391ebe944fabf5fcfb048f023862
Miro Tunk <miro.tunk@icarus-consult.de>
2019-02-14 17:01:22 +0100

    feat: add cached hive

---
+++ src/Xive/Hive/CachedHive.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+using Xive.Comb;
+using Yaapii.Atoms.Enumerable;
+
+namespace Xive.Hive
+{
+    /// <summary>
+    /// A cached hive.
+    /// </summary>
+    /// <param name="origin"></param>
+    public sealed class CachedHive : IHive
+    {
+        private readonly IDictionary<string, byte[]> binMemory;
+        private readonly IDictionary<string, XNode> xmlMemory;
+        private readonly string name;
+        private readonly IHive origin;
+
+        /// <summary>
+        /// A cached hive.
+        /// By using this ctor, the contents of the hive will live as long as this instance lives.
+        /// </summary>
+        /// <param name="origin"></param>
+        public CachedHive(string name, IHive origin) : this(name, origin, new Dictionary<string,byte[]>(), new Dictionary<string, XNode>())
+        { }
+
+        /// <summary>
+        /// A cached hive.
+        /// By using this ctor, the contents of the hive will live in the injected memories.
+        /// </summary>
+        /// <param name="origin"></param>
+        public CachedHive(string name, IHive origin, IDictionary<string, byte[]> binMemory, IDictionary<string, XNode> xmlMemory)
+        {
+            this.binMemory = binMemory;
+            this.xmlMemory = xmlMemory;
+            this.origin = origin;
+            this.name = name;
+        }
+
+        public IEnumerable<IHoneyComb> Combs(string xpath)
+        {
+            return
+                new Mapped<IHoneyComb, IHoneyComb>(
+                    comb => 
+                        new CachedComb(
+                            $"{this.name}{Path.DirectorySeparatorChar}{comb.Name()}", 
+                            comb, 
+                            this.binMemory, 
+                            this.xmlMemory
+                        ),
+                    this.origin.Combs(xpath)
+                );
+        }
+
+        public IHoneyComb HQ()
+        {
+            return 
+                new CachedComb(
+                    this.origin.HQ().Name(), 
+                    this.origin.HQ(), 
+                    this.binMemory, 
+                    this.xmlMemory
+                );
+        }
+
+        public string Name()
+        {
+            return this.origin.Name();
+        }
+    }
+}
---
+++ tests/Test.Xive/Hive/CachedHiveTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+using Xive.Comb;
+using Xive.Test;
+using Xive.Xocument;
+using Xunit;
+using Yaapii.Atoms.Scalar;
+
+namespace Xive.Hive.Test
+{
+    public sealed class CachedHiveTests
+    {
+        [Fact]
+        public void ReadsBinaryOnce()
+        {
+            var binCache = new Dictionary<string, byte[]>();
+            var xmlMemory = new Dictionary<string, XNode>();
+            int reads = 0;
+            var hive =
+                new CachedHive(
+                    "phonebook",
+                    new SimpleHive("phonebook",
+                        combName =>
+                        new SimpleComb(
+                            "my-comb",
+                            cellname =>
+                                new FkCell(
+                                    content => { },
+                                    () =>
+                                    {
+                                        reads++;
+                                        return new byte[0];
+                                    }
+                                ),
+                            (x, c) => new CachedXocument(x, new SimpleXocument("catalog"), xmlMemory)
+                        )
+                    ),
+                    binCache,
+                    xmlMemory
+                );
+
+            new Catalog(hive).Create("123");
+
+            var cell =
+                new FirstOf<IHoneyComb>(
+                    hive.Combs("@id='123'")
+                )
+                .Value()
+                .Cell("adalbert");
+
+            cell.Content();
+            cell.Content();
+
+            Assert.Equal(1, reads);
+        }
+    }
+}
