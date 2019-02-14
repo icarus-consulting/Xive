a4b6f06b4fd119802c5c542693228380cd42f599
Miro Tunk <miro.tunk@icarus-consult.de>
2019-02-14 17:01:32 +0100

    feat: add cached farm

---
+++ src/Xive/Farm/CachedFarm.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+using Xive.Hive;
+
+namespace Xive.Farm
+{
+    /// <summary>
+    /// A farm that is cached.
+    /// </summary>
+    public class CachedFarm : IFarm
+    {
+        private readonly IFarm origin;
+        private readonly IDictionary<string, byte[]> binMemory;
+        private readonly IDictionary<string, XNode> xmlMemory;
+
+        /// <summary>
+        /// A farm that is cached.
+        /// </summary>
+        /// <param name="origin"></param>
+        public CachedFarm(IFarm origin) : this(origin, new Dictionary<string,byte[]>(), new Dictionary<string,XNode>())
+        { }
+
+        /// <summary>
+        /// A farm that is cached.
+        /// </summary>
+        public CachedFarm(IFarm origin, IDictionary<string,byte[]> binMemory, IDictionary<string,XNode> xmlMemory)
+        {
+            this.origin = origin;
+            this.binMemory = binMemory;
+            this.xmlMemory = xmlMemory;
+        }
+
+        public IHive Hive(string name)
+        {
+            return
+                new CachedHive(
+                    new Coordinate(name).AsString(),
+                    this.origin.Hive(name),
+                    this.binMemory,
+                    this.xmlMemory
+                );
+        }
+    }
+}
---
+++ tests/Test.Xive/Farm/CachedFarmTests.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+using Xive.Farm;
+using Xive.Hive;
+using Xunit;
+using Yaapii.Atoms.Scalar;
+using Yaapii.Xambly;
+
+namespace Xive.Test.Farm
+{
+    public sealed class CachedFarmTests
+    {
+        [Fact]
+        public void FillsXmlCache()
+        {
+            var binMemory = new Dictionary<string, byte[]>();
+            var xmlMemory = new Dictionary<string, XNode>();
+
+            var farm = 
+                new CachedFarm(
+                    new RamFarm(),
+                    binMemory,
+                    xmlMemory
+                );
+
+            new Catalog(
+                farm.Hive("umbrella-corp")
+            ).Create("1024");
+
+            var comb =
+                new FirstOf<IHoneyComb>(
+                    farm.Hive("umbrella-corp").Combs("@id='1024'")
+                )
+                .Value();
+
+            comb.Xocument("some.xml")
+                .Modify(
+                    new Directives().Xpath("/some").Add("item")
+                );
+
+            for (int i = 0; i < 10000; i++)
+            {
+                comb.Xocument("some.xml").Node();
+            }
+
+            Assert.Contains("umbrella-corp\\1024\\some.xml", xmlMemory.Keys);
+        }
+
+        [Fact]
+        public void FillsBinCache()
+        {
+            var binMemory = new Dictionary<string, byte[]>();
+            var xmlMemory = new Dictionary<string, XNode>();
+
+            var farm =
+                new CachedFarm(
+                    new RamFarm(),
+                    binMemory,
+                    xmlMemory
+                );
+
+            new Catalog(
+                farm.Hive("umbrella-corp")
+            ).Create("1024");
+
+            var comb =
+                new FirstOf<IHoneyComb>(
+                    farm.Hive("umbrella-corp").Combs("@id='1024'")
+                )
+                .Value();
+
+            comb.Cell("some.xml");
+
+            for (int i = 0; i < 10000; i++)
+            {
+                comb.Cell("some.xml");
+            }
+
+            Assert.Contains("umbrella-corp\\1024\\some.xml", binMemory.Keys);
+        }
+    }
+}
