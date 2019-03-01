28d02df4d66c8f331e96ce1d7927501a02e58e11
Miro Tunk <miro.tunk@icarus-consult.de>
2019-03-01 10:46:21 +0100

    fix: add missing usings for cell to unblock

--- src/Xive/Xocument/CellXocument.cs
+++ src/Xive/Xocument/CellXocument.cs
@@ -32,51 +32,56 @@ namespace Xive.Xocument
     /// A xocument which is stored in a cell.
     /// </summary>
     public sealed class CellXocument : XocumentEnvelope
-    { 
+    {
         /// <summary>
         /// A xocument which is stored in a cell.
         /// </summary>
         public CellXocument(ICell cell, string name) : base(new ScalarOf<IXocument>(() =>
             {
-                return
-                    new SimpleXocument(
-                        new ScalarOf<XNode>(() =>
-                        {
-                            if (name.ToLower().EndsWith(".xml"))
-                            {
-                                name = name.Substring(0, name.Length - 4);
-                            }
-                            name = Path.GetFileName(name);
-                            byte[] content = cell.Content();
-                            if(content.Length == 0)
+                lock (cell)
+                {
+                    return
+                        new SimpleXocument(
+                            new ScalarOf<XNode>(() =>
                             {
                                 using (cell)
                                 {
-                                    cell.Update(
-                                        new InputOf(
-                                            new XDocument(
-                                                new XElement(name)
-                                            ).ToString()
-                                        )
-                                    );
-                                    content = cell.Content();
+                                    if (name.ToLower().EndsWith(".xml"))
+                                    {
+                                        name = name.Substring(0, name.Length - 4);
+                                    }
+                                    name = Path.GetFileName(name);
+
+                                    byte[] content = cell.Content();
+                                    if (content.Length == 0)
+                                    {
+
+                                        cell.Update(
+                                            new InputOf(
+                                                new XDocument(
+                                                    new XElement(name)
+                                                ).ToString()
+                                            )
+                                        );
+                                        content = cell.Content();
+                                    }
+                                    return
+                                        XDocument.Parse(
+                                            new TextOf(
+                                                new InputOf(content)
+                                            ).AsString()
+                                        );
+                                }
+                            }),
+                            xnode =>
+                            {
+                                using (cell)
+                                {
+                                    cell.Update(new InputOf(xnode.ToString()));
                                 }
                             }
-                            return
-                                XDocument.Parse(
-                                    new TextOf(
-                                        new InputOf(content)
-                                    ).AsString()
-                                );
-                        }),
-                        xnode =>
-                        {
-                            using (cell)
-                            {
-                                cell.Update(new InputOf(xnode.ToString()));
-                            }
-                        }
-                    );
+                        );
+                }
             })
         )
         { }
