89102184415cd6f75d69df7ba173f07b4fcc9044
DFU398 <dominik.fuchs@icarus-consult.de>
2019-07-15 09:39:07 +0200

    fix: CachedHive shifted passes blacklist

--- tests/Test.Xive/Hive/CachedHiveTests.cs
+++ tests/Test.Xive/Hive/CachedHiveTests.cs
@@ -260,6 +260,53 @@ namespace Xive.Hive.Test
         }
 
         [Fact]
+        public void BlackListsShiftedBinaries()
+        {
+            var binCache = new Dictionary<string, MemoryStream>();
+            var xmlMemory = new Dictionary<string, XNode>();
+            int reads = 0;
+            var hive =
+                new CachedHive(
+                    new SimpleHive("phonebook",
+                        combName =>
+                        new SimpleComb(
+                            "my-comb",
+                            cellname =>
+                                new FkCell(
+                                    content => { },
+                                    () =>
+                                    {
+                                        reads++;
+                                        return new byte[0];
+                                    }
+                                ),
+                            (x, c) => new CachedXocument(x, new SimpleXocument("catalog"), xmlMemory, binCache)
+                        )
+                    ),
+                    binCache,
+                    xmlMemory,
+                    new List<string>()
+                    {
+                        "*dal*rt*"
+                    }
+                ).Shifted("hello");
+
+            new MutexCatalog(hive).Create("123");
+
+            var cell =
+                new FirstOf<IHoneyComb>(
+                    hive.Combs("@id='123'")
+                )
+                .Value()
+                .Cell("adalbert");
+
+            cell.Content();
+            cell.Content();
+
+            Assert.Equal(2, reads);
+        }
+
+        [Fact]
         public void ShiftingRamHiveIncludesScope()
         {
             var binCache = new Dictionary<string, MemoryStream>();
