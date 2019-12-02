83d7812a786220a27085edb26ff7a7dce90ac930
jaanpeeter <jan-peter.schulz@icarus-consult.de>
2019-12-02 16:36:10 +0100

    fix: AsString instead of toString

--- tests/Test.Xive/Hive/FileHiveTests.cs
+++ tests/Test.Xive/Hive/FileHiveTests.cs
@@ -130,7 +130,7 @@ namespace Xive.Hive.Test
                                     "product",
                                     "2CV"
                                 )
-                            ).ToString()                        
+                            ).AsString()                        
                         )
                     );
                 }
